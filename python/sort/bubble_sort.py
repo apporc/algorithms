2d04a7a259989a50710e93b45c721eed5402b353
apporc <appleorchard2000@gmail.com>
2017-02-17 17:09:50 +0800

    another bubble sort

--- python/sort/bubble_sort.py
+++ python/sort/bubble_sort.py
@@ -1,7 +1,18 @@
-def bubble_sort(array):
+def bubble_sort1(array):
     if not isinstance(array, list):
         return False
     for i in reversed(xrange(1, len(array))):
         for j in xrange(0, i):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
+
+
+def bubble_sort(array):
+    if not isinstance(array, list):
+        return False
+    i = len(array) - 1
+    while i > 0:
+        for j in xrange(0, i):
+            if array[j] > array[i]:
+                array[j], array[i] = array[i], array[j]
+        i -= 1
